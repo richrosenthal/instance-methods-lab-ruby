5d97ee54b940f243b51eb649bc928344db4b4b38
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-11 17:32:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,17 @@
 class Person
+  def talk 
+    puts "Hello World!"
+  end
+  def walk 
+    puts "The Person is walking"
+  end
 end
 
 adele_goldberg = Person.new 
 alan_kay = Person.new
+ada = Person.new
+
+ada.talk
+ada.walk 
+
 
